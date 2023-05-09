b7f5bb1d6225e354b218deb7ac9f3d72045eb40d
hostdog01 <hostdog01@naver.com>
2023-05-09 08:48:59 +0000

    fix

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const PORT = 8080;
 //This is APP11
 const app = express();
 
-app.use("", (req, res, next) =&gt; {
+app.use("", (req, res, next) => {
   console.log("req: ", req);
   console.log("res: ", res);
   next()
 });
 
-app.get("/", (req, res) =&gt; {
+app.get("/", (req, res) => {
   res.send("hello krampoline!!!!!");
 });
 
-app.get("/list", (req, res) =&gt; {
+app.get("/list", (req, res) => {
   res.send("list-endpoint");
 });
 
